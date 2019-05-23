c37a0882068853a5ab97b4d48fe0998d9390cc68
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-23 15:21:49 -0400

    Refactored

--- my_first_web_app/my_first_web_app/urls.py
+++ my_first_web_app/my_first_web_app/urls.py
@@ -13,44 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from random import randint
-from django.http import HttpResponse
-from django.shortcuts import render
+
 from django.urls import path
+from my_first_web_app import views
 
-
-def home_page(request):
-    context = {'name': 'Betty Maker'}
-    response = render(request, 'index.html', context)
-    return HttpResponse(response)
-
-def portfolio_page(request):
-    image_urls = []
-    for i in range(5):
-        random_number = randint(0, 100)
-        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
-    context = {'gallery_images': image_urls}    
-    response = render(request, 'portfolio.html', context)
-    return HttpResponse(response)
-
-def about_me_page(request):
-    context = {
-        'skills': ['html', 'css', 'python'],
-        'interests': ['web development', 'responsive design']
-         }
-    response = render(request, 'about_me.html', context)
-    return HttpResponse(response)
-
-def favourites_page(request):
-    context = {
-        'fave_links': ['https://gitignore.io/']
-    }
-    response = render(request, 'favourites.html', context)
-    return HttpResponse(response)
 
 urlpatterns = [
-    path('home/', home_page),
-    path('portfolio/', portfolio_page),
-    path('about_me/', about_me_page),
-    path('favourites/', favourites_page)
+    path('home/', views.home_page),
+    path('portfolio/', views.portfolio_page),
+    path('about_me/', views.about_me_page),
+    path('favourites/', views.favourites_page)
 ]
---
+++ my_first_web_app/my_first_web_app/views.py
@@ -0,0 +1,35 @@
+from random import randint
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    context = {"name": "Betty Maker"}
+    response = render(request, "index.html", context)
+    return HttpResponse(response)
+
+
+def portfolio_page(request):
+    image_urls = []
+    for i in range(5):
+        random_number = randint(0, 100)
+        image_urls.append(
+            "https://picsum.photos/400/600/?image={}".format(random_number)
+        )
+    context = {"gallery_images": image_urls}
+    response = render(request, "portfolio.html", context)
+    return HttpResponse(response)
+
+
+def about_me_page(request):
+    context = {
+        "skills": ["html", "css", "python"],
+        "interests": ["web development", "responsive design"],
+    }
+    response = render(request, "about_me.html", context)
+    return HttpResponse(response)
+
+
+def favourites_page(request):
+    context = {"fave_links": ["https://gitignore.io/"]}
+    response = render(request, "favourites.html", context)
+    return HttpResponse(response)
